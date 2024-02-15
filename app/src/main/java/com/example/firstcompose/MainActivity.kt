8082e702dac7702130ab2f81661d4478c15f1402
Anshu Singh <anshusingh7738@gmail.com>
2024-02-15 23:58:55 +0530

    exploring other composable function

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -3,26 +3,34 @@ package com.example.firstcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.firstcompose.ui.theme.FirstComposeTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Anshu singh My First Compose Project")
+            Greeting()
 
         }
     }
 
+    @Preview(showBackground = true, widthDp = 100, heightDp = 500)
     @Composable
-    fun Greeting(name: String)  {
-        Text(text =  "Hello $name!")
+    fun Greeting() {
+        Text(
+            text = "Hello Anshu Singh !",
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red,
+            fontSize = 36.sp,
+            textAlign = TextAlign.Center
+        )
     }
 }
