f5f29eed6f3e20fd2d39d17a05a64fa2e0eadc68
anshu <anshu@merchantech.com>
2024-02-16 15:45:53 +0530

    update: learning row amd column

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -3,34 +3,93 @@ package com.example.firstcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting()
-
+            LayoutContainer()
         }
     }
 
-    @Preview(showBackground = true, widthDp = 100, heightDp = 500)
     @Composable
-    fun Greeting() {
-        Text(
-            text = "Hello Anshu Singh !",
-            fontStyle = FontStyle.Italic,
-            fontWeight = FontWeight.Bold,
-            color = Color.Red,
-            fontSize = 36.sp,
-            textAlign = TextAlign.Center
-        )
+    fun LayoutContainer() {
+        Column {
+            RowArrangement()
+            ColumnArrangement()
+        }
+    }
+
+    @Preview(showSystemUi = true, showBackground = false)
+    @Composable
+    fun RowArrangement() {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+
+            Text(
+                text = "Hello Anshu Singh 1!",
+                Modifier.background(Color.Red),
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center
+            )
+
+            Text(
+                text = "Hello Anshu Singh 2!",
+                Modifier.background(Color.Green),
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold,
+                color = Color.Red,
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = "Hello Anshu Singh 3!",
+                Modifier.background(Color.Blue),
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold,
+                color = Color.Red,
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+
+    @Preview(showSystemUi = true, showBackground = false)
+    @Composable
+    fun ColumnArrangement() {
+        Column(
+            modifier = Modifier.fillMaxHeight().fillMaxWidth(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Column Text 1", Modifier.background(Color.Green))
+            Text(text = "Column Text 2", Modifier.background(Color.Blue))
+            Text(text = "Column Text 3", Modifier.background(Color.Red))
+        }
     }
 }
